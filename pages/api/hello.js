83a8d4d56a437d4eb6e8cbbba8c1a233ab9f9058
Caroline Julliat <carolinejulliat@gmail.com>
2023-07-18 09:18:34 +0200

    tuto completed

---
+++ pages/api/hello.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  res.status(200).json({ text: "Hello" })
+}
+
+// Try accessing it at http://localhost:3000/api/hello. You should see {"text":"Hello"}. Note that:
+
+// req is an instance of http.IncomingMessage, plus some pre-built middlewares.
+// res is an instance of http.ServerResponse, plus some helper functions.
